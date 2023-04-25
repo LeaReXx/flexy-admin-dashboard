fdefa80e646250f3e9dec2daf64c870bbbfee150
Sepehr Aghapour <dev.sepehr@outlook.com>
2023-04-25 16:34:40 +0330

    useless logs deleted

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log(import.meta.env.VITE_SUPABASE_URL);
-console.log(import.meta.env.VITE_SUPABASE_ANON_KEY);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
