482aefea6c0cc54c49c041e66674418d6dd27588
Sepehr Aghapour <dev.sepehr@outlook.com>
2023-04-29 18:06:55 +0330

    auto redirect user to dashboard after login

--- src/custom hooks/useLoginUser.js
+++ src/custom hooks/useLoginUser.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
 import addNotifToDom from "../components/notifications/Notifications.jsx";
-import { Navigate, redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const useLoginUser = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const loginToSupabase = async (email, password) => {
     setLoading(true);
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -15,7 +15,9 @@ const useLoginUser = () => {
     });
     if (data.user) {
       addNotifToDom("You have successfully logged in", "success");
-      return redirect('/dashboard')
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 2000);
     } else {
       addNotifToDom("email or password is incorrect", "error");
     }
