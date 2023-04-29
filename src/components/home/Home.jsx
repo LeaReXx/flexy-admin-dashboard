13568b3ae1603e2ef6d248af67516cb2597b9d39
Sepehr Aghapour <dev.sepehr@outlook.com>
2023-04-29 18:33:10 +0330

    Log out user completed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import useAuth from "../../custom hooks/useAuth";
+import useLogout from "../../custom hooks/useLogout";
 export default function Home() {
   const [authorizationUser, siteUser] = useAuth();
+  const [logOutUser] = useLogout();
   useEffect(() => {
     authorizationUser();
   }, []);
-  
+
   return (
     <>
       {siteUser ? (
@@ -16,6 +18,13 @@ export default function Home() {
               Dashboard
             </button>
           </NavLink>
+
+          <button
+            className="border border-[#585858] hover:bg-[#585858] hover:text-white py-1 px-3 rounded-md"
+            onClick={() => logOutUser()}
+          >
+            Log Out
+          </button>
         </div>
       ) : (
         <div className="flex gap-3 justify-center my-5">
---
+++ src/custom hooks/useLogout.js
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabaseClient";
+import addNotifToDom from "../components/notifications/Notifications";
+const useLogout = () => {
+  const navigate = useNavigate();
+
+  const logOutUser = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (!error) {
+      addNotifToDom("You are logged out", "success");
+
+      location.reload();
+    } else {
+      addNotifToDom("something went wrong, please try again", "error");
+    }
+  };
+  return [logOutUser];
+};
+
+export default useLogout;
