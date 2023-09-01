e0c00becb09a57eb2b6e4c97bfbb61daa58c0751
Sepehr Aghapour <petrol_cs@yahoo.com>
2023-09-01 19:27:56 +0330

    `Dashboard` check user is logged in & logout btn fixed

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,32 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import logo from "../../assets/imgs/logo.svg";
-import AuthContext from "../../context/AuthContext";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Dashboard() {
-  const authContext = useContext(AuthContext);
+  const { logOutUser, isLoggedIn } = useAuth();
 
-
-  useEffect(()=>{
-      console.log(authContext.isLoggedIn)
-  },[])
-  return authContext.isLoggedIn ? (
+  return isLoggedIn ? (
     <div className="flex justify-between p-5">
       <div>
-
         <img src={logo} alt="Logo" />
       </div>
       <button
         className="border border-[#585858] hover:bg-[#585858] hover:text-white py-1 px-3 rounded-md"
-        onClick={() => authContext.logOut()}
+        onClick={() => logOutUser()}
       >
         Log out
       </button>
     </div>
   ) : (
     <>
-            {console.log(authContext)}
-    <Navigate to="/" />
+      <Navigate to="/" />
     </>
   );
 }
