cc69ebf59a58d29512b74bd68b2e7bd897d91562
Sepehr Aghapour <petrol_cs@yahoo.com>
2023-09-04 12:28:47 +0330

    check user login on page load & import navbar

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,26 +1,28 @@
-import React from "react";
-import logo from "../../assets/imgs/logo.svg";
-import { Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import Navbar from "../../components/navbar/Navbar";
 
 export default function Dashboard() {
   const { logOutUser, isLoggedIn } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, []);
 
-  return isLoggedIn ? (
-    <div className="flex justify-between p-5">
-      <div>
-        <img src={logo} alt="Logo" />
-      </div>
-      <button
-        className="border border-[#585858] hover:bg-[#585858] hover:text-white py-1 px-3 rounded-md"
-        onClick={() => logOutUser()}
-      >
-        Log out
-      </button>
-    </div>
-  ) : (
+  return (
     <>
-      <Navigate to="/" />
+      <Navbar />
+      <div className="flex justify-between p-5">
+        <button
+          className="border border-[#585858] hover:bg-[#585858] hover:text-white py-1 px-3 rounded-md"
+          onClick={() => logOutUser()}
+        >
+          Log out
+        </button>
+      </div>
     </>
   );
 }
