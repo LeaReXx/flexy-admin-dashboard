69da940fde626f7b51e147c2a43460965ff263cd
Sepehr Aghapour <petrol_cs@yahoo.com>
2023-09-15 15:42:24 +0330

    add `z-index` to `SideMenu`

--- src/components/side menu/SideMenu.jsx
+++ src/components/side menu/SideMenu.jsx
@@ -1,6 +1,6 @@
 const SideMenu = ({ toggleMobileSideMenu, mobileSideMenu }) => {
   return (
-    <>
+    <div className="z-30">
       <div
         className={`w-screen h-screen fixed top-0 xl:hidden bg-[#49494970] ${
           mobileSideMenu ? "opacity-100 visible" : "invisible opacity-0"
@@ -8,7 +8,7 @@ const SideMenu = ({ toggleMobileSideMenu, mobileSideMenu }) => {
         onClick={toggleMobileSideMenu}
       ></div>
       <article className={`bg-[#5900ff] h-screen w-[250px] fixed xl:translate-x-0 xl:static xl:visible top-0 left-0 duration-300 ease ${mobileSideMenu ? 'translate-x-0 visible': '-translate-x-[400px] invisible'}`}></article>
-    </>
+    </div>
   );
 };
 
